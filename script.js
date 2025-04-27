cb2b5e2d309399ecd846a99cde27a35a00159666
Robby-han <bihanr1309@gmail.com>
2025-04-27 17:47:39 +0700

    end styling list add and start scripting it

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const add = document.querySelector('.add')
+const add = document.querySelectorAll(".add");
+
+add.forEach((e) => {
+  e.addEventListener("click", function () {
+    // add class
+    const addContainer = document.querySelector(".add-container");
+    addContainer.classList.add("active");
+    // remove class
+    const closeButton = document.querySelector(".close");
+    closeButton.addEventListener("click", function () {
+      addContainer.classList.remove("active");
+    });
+  });
+});
+
