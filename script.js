56fd1c3ce2bad7786749f17201d8f4f220d81351
Robby-han <bihanr1309@gmail.com>
2025-05-01 23:12:57 +0700

    done scripting for list

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const add = document.querySelectorAll(".add");
 const addContainer = document.querySelector(".add-container");
+const bColor = document.querySelectorAll(".background-color");
 
 add.forEach((e) => {
   e.addEventListener("click", function () {
@@ -13,20 +14,42 @@ add.forEach((e) => {
   });
 });
 
+let selectedColor = ""
+
+bColor.forEach((e) => {
+  e.addEventListener("click", function () {
+    const colors = [
+      "red",
+      "yellow",
+      "orange",
+      "blue",
+      "green",
+      "magenta",
+      "indigo",
+      "brown",
+    ];
+
+    colors.forEach((color) => addContainer.classList.remove(color));
+    colors.forEach((color) => {
+      if (e.classList.contains(color)) {
+        addContainer.classList.add(color);
+        selectedColor = color
+      }
+    });
+  });
+});
+
 const toDo = document.getElementById("toDo");
 const check = document.querySelector(".check");
 
 check.addEventListener("click", function () {
-  const ul = document.querySelectorAll(".ul");
   const array = toDo.value.split(/\r?\n/);
 
-  ul.forEach((e) => {
-    array.map((item) => {
-      const list = document.createElement("li");
-      const textList = document.createTextNode(item);
-      list.appendChild(textList);
-      e.appendChild(list);
-    });
+  const listItem = array.map((item) => {
+    const list = document.createElement("li");
+    const textList = document.createTextNode(item);
+    list.appendChild(textList);
+    return item;
   });
 
   // get the date input
@@ -34,7 +57,7 @@ check.addEventListener("click", function () {
     const year = dateObj.getFullYear();
     const month = (dateObj.getMonth() + 1).toString().padStart(2, "0");
     const day = dateObj.getDate().toString().padStart(2, "0");
-    return `${year}-${month}-${day}`;
+    return `${day}/${month}/${year}`;
   };
 
   const calculateDate = (offset) => {
@@ -43,49 +66,76 @@ check.addEventListener("click", function () {
     return formatDate(dateObj);
   };
 
-  const date = document.getElementById("date").value;
+  const dateInput = document.getElementById("date").value;
   const today = calculateDate(0);
   const yesterday = calculateDate(-1);
   const tomorrow = calculateDate(1);
 
-  if (date === today) {
-    console.log("Today");
-  } else if (date === yesterday) {
-    console.log("Yesterday");
-  } else if (date === tomorrow) {
-    console.log("Tomorrow");
+  let formattedDate = "";
+  if (dateInput) {
+    const dateParts = dateInput.split("-");
+    formattedDate = `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
   }
-  
+
+  let dateResult = "";
+  if (formattedDate === today) {
+    dateResult = "Today";
+  } else if (formattedDate === yesterday) {
+    dateResult = "Yesterday";
+  } else if (formattedDate === tomorrow) {
+    dateResult = "Tomorrow";
+  }
+
+  const resultObject = {
+    date: formattedDate,
+    day: dateResult,
+    tasks: listItem,
+    color: selectedColor,
+  };
+
+  addList(resultObject);
+
   addContainer.classList.remove("active");
 });
 
-function card(m) {
-  return `<div class="card card-small">
-                <div class="header-1">
-                  <h3 class="date">04/04/2025</h3>
-                  <a class="edit-list">ini edit</a>
+function addList(items) {
+  const list = cards(items);
+  const tempDiv = document.createElement("div");
+  tempDiv.innerHTML = list;
+
+  const cardBody = document.querySelectorAll(".box-container");
+  cardBody.forEach((e) => {
+    while (tempDiv.firstChild) {
+      e.appendChild(tempDiv.firstChild);
+    }
+  });
+}
+
+function cards(item) {
+  const tasksHTML = item.tasks
+    .map((task) => `<li>${task}<input type="checkbox" id="checkbox"></li>`)
+    .join("");
+  let size = "";
+  if (item.tasks.length < 6) {
+    size = "card-small";
+  } else if (item.tasks.length >= 6 && item.tasks.length <= 8) {
+    size = "card-medium";
+  } else if (item.tasks.length > 8) {
+    size = "card-large";
+  }
+  return `<div class="card ${size} ${item.color}">
+                  <div class="header-1">
+                    <h3 class="date">${item.date}</h3>
+                    <a class="edit-list">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="white"><path d="M80 0v-160h800V0H80Zm160-320h56l312-311-29-29-28-28-311 312v56Zm-80 80v-170l448-447q11-11 25.5-17t30.5-6q16 0 31 6t27 18l55 56q12 11 17.5 26t5.5 31q0 15-5.5 29.5T777-687L330-240H160Zm560-504-56-56 56 56ZM608-631l-29-29-28-28 57 57Z"/></svg>
+                    </a>
+                  </div>
+                  <div class="header-2">
+                    <h1 class="day">${item.day}</h1>
+                  </div>
+                  <ul>
+                    ${tasksHTML}
+                  </ul>
                 </div>
-                <div class="header-2">
-                  <h1 class="day">Today</h1>
-                </div>
-                <ul>
-                  <li>
-                    Lorem ipsum dolor sit amet.
-                    <input type="checkbox" id="checkbox">
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet.
-                    <input type="checkbox" id="checkbox">
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet.
-                    <input type="checkbox" id="checkbox">
-                  </li>
-                  <li>
-                    Lorem ipsum dolor sit amet.
-                    <input type="checkbox" id="checkbox">
-                  </li>
-                </ul>
-              </div>
-            </div>`;
+              </div>`;
 }
